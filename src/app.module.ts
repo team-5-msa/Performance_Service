6f22c0664df20958e76f566c41c5852ef1fbe14a
Hayeon Baek <bhy0512@gmail.com>
2025-12-08 02:24:13 +0900

    chore: TypeORM 연결 설정 최적화

--- src/app.module.ts
+++ src/app.module.ts
@@ -14,7 +14,7 @@ import { ReservationsModule } from './reservations/reservations.module';
     TypeOrmModule.forRoot({
       type: 'postgres',
       host: process.env.DB_HOST,
-      port: parseInt(process.env.DB_PORT || '5432'),
+      port: parseInt(process.env.DB_PORT || '6543'),
       username: process.env.DB_USERNAME,
       password: process.env.DB_PASSWORD,
       database: process.env.DB_DATABASE,
@@ -24,6 +24,11 @@ import { ReservationsModule } from './reservations/reservations.module';
         rejectUnauthorized: false,
       },
       logging: true,
+      extra: {
+        max: 1,
+        connectionTimeoutMillis: 5000,
+        idleTimeoutMillis: 30000,
+      },
     }),
     PerformancesModule,
     ReservationsModule,
