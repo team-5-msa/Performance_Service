651ae889a05fff978e7fc1ac715eaed7204ed17b
Hayeon Baek <bhy0512@gmail.com>
2025-12-08 00:53:48 +0900

    chore: /docs 제거

--- src/main.ts
+++ src/main.ts
@@ -25,7 +25,7 @@ async function bootstrap() {
     .build();
 
   const documentFactory = () => SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api/docs', app, documentFactory);
+  SwaggerModule.setup('api', app, documentFactory);
 
   await app.listen(process.env.PORT ?? 3000);
 }
